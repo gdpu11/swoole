a6ee2debeb01942549915fd237474da235dc6117
liqiang <455019211@qq.com>
2017-08-17 18:00:01 +0800

    1

--- TcpServer.php
+++ TcpServer.php
@@ -23,7 +23,7 @@ spl_autoload_register('autoLoad');
 
 //创建Server对象，监听 *:9501端口
 $serv = new swoole_server("0.0.0.0", 9501);
-RedisUtil::set(1,2,3);
+RedisUtil::set(1,2);
 
 $serv->set(array(
     'worker_num' => 8,   //工作进程数量
