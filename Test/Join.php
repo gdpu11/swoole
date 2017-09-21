4d582d09dffc891f5bb823b34697d44c8a67efc2
liqiang <455019211@qq.com>
2017-09-21 11:56:32 +0800

    1

--- Test/Join.php
+++ Test/Join.php
@@ -10,11 +10,25 @@ use Utils\Common;
 class Join
 {
 
+
 	//php G:\nginx\swoole\cli.php Test getAliCli
 	public static function getAliCli(){
-
+		$one = array(
+			'id'=>1,
+			'company'=>1,
+			'name'=>1,
+			'hot'=>1,
+			'url'=>1,
+			'city'=>1,
+			'main'=>1,
+			'mode'=>1,
+			'add_time'=>1,
+			);
+		ali::add($one);
+		print_r(ali::getSums($one,'hot'));
 		print_r(ali::getCount());
-		exit();
+		print_r(ali::getAll());
+		ali::delete($one);
 		// ali::setTbFields();
 		// ali::ifTbField();
 	}
