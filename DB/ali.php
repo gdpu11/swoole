c7d345dbafa0923be7dcbfb42358426cc5fda3ca
liqiang <455019211@qq.com>
2017-09-25 10:00:41 +0800

    1

--- DB/ali.php
+++ DB/ali.php
@@ -16,13 +16,13 @@ class ali extends PdoOperateBase
 {
       protected static $SCHEMA = array(
 			'TABLENAME'=>'ali',
-	        'PARTITION'=> array(
-	          'suffix' => '_',// 要分表的后缀比如 tablename_中的'_'
-	          'field' => 'id',// 要分表的字段 通常数据会根据某个字段的值按照规则进行分表,我们这里按照用户的id进行分表
-	          'type' => 'mod',// 分表的规则 包括id year mod md5 函数 和首字母，此处选择mod（求余）的方式
-	          'num' => '10',// 分表的数目 可选 实际分表的数量，在建表阶段就要确定好数量，后期不能增减表的数量
-	          'default' => 1,// 默认下标
-	        ),
+	        // 'PARTITION'=> array(
+	        //   'suffix' => '_',// 要分表的后缀比如 tablename_中的'_'
+	        //   'field' => 'id',// 要分表的字段 通常数据会根据某个字段的值按照规则进行分表,我们这里按照用户的id进行分表
+	        //   'type' => 'mod',// 分表的规则 包括id year mod md5 函数 和首字母，此处选择mod（求余）的方式
+	        //   'num' => '10',// 分表的数目 可选 实际分表的数量，在建表阶段就要确定好数量，后期不能增减表的数量
+	        //   'default' => 1,// 默认下标
+	        // ),
 	    );
 
 }
